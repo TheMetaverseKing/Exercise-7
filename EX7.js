b7ae4f74d95c687f013083319809356bc4fc5d81
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-04 00:29:15 +0500

    Add files via upload

---
+++ EX7.js
@@ -0,0 +1,6 @@
+"use strict";
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log(7 + 1);
+console.log(1212445 - 1212437);
+console.log(1 * 8);
+console.log(789320 / 98665);
